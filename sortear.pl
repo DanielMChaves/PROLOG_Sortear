c78805a29071bf8b24e4fca56b13c381c682d11c
DanielMChaves <daniel.melero.95@gmail.com>
2017-05-18 01:23:41 +0200

    Delete Elements SemiWorking

--- sortear.pl
+++ sortear.pl
@@ -1,5 +1,7 @@
 :-module(_,_).
 
+% Modo de Uso
+%
 % Ini ----- IN
 % C ------- IN
 % N ------- OUT
@@ -59,38 +61,75 @@ productoCartesianoAux([H1|T1],[H2|T2],[[H1,H2]|T3],L4):-
 % obtenerSolucion/6: Genera y comprueba las soluciones
 obtenerSolucion(Ini,Fin,[[P,N]|_],TFin,P,N) :-
   prepararLista(Ini,P,N,TFin,Posible),
-  Fin == Posible.
+  Fin == Posible, !.
 obtenerSolucion(Ini,Fin,[_|LCs],TFin,P,N) :-
   obtenerSolucion(Ini,Fin,LCs,TFin,P,N).
 obtenerSolucion(_,_,[],_,_,_) :-
   fail.
 
+% HASTA AQUI BIEN
 % prepararLista/5: Genera listas de soluciones
-% SOLO FUNCIONA CUANDO LAS LISTAS TIENEN LETRAS
 prepararLista(Ini,P,N,TFin,Posible) :-
-  seleccionarPosicion(Ini,P,LP1,LP2),
-  append(LP2,LP1,LP),
-  eliminarElementos(LP,LP,N,N,TFin,PosibleDesordenado),
-  sort(PosibleDesordenado,Posible).
+  seleccionarPosicion(Ini,P,LP),
+  eliminarElementos(Ini,LP,N,N,TFin,Posible).
 
 % seleccionarPosicion/3: Genera una lista con la posicion dada
-seleccionarPosicion(Ini,0,[],Ini).
-seleccionarPosicion([CI|CIs],P,[CI|LP1s],LP2) :-
+seleccionarPosicion(Ini,1,Ini).
+seleccionarPosicion([_|CIs],P,LP2) :-
   Nuevo is P - 1,
-  seleccionarPosicion(CIs,Nuevo,LP1s,LP2).
+  seleccionarPosicion(CIs,Nuevo,LP2).
 
-% FALLA AQUÍ
+% Posible Backtraking
+% CASO: Contador a 0
+eliminarElementos(LP1,[E|LP2s],N,1,TFin,PD) :-
+  eliminarElegido(LP1,[E|LP2s],LP1Mitad),
+  append(LP1Mitad,LP2s,LP),
+  eliminarElementos(LP,LP2s,N,N,TFin,PD).
+% CASO: Fin del Eliminado
+eliminarElementos(CLP,LP,_,N,TFin,CLP) :-
+  LP \= [],
+  N =\= 1,
+  length(CLP,TCLP),
+  TCLP =:= TFin, !.
 % CASO: Lista vacia
 eliminarElementos(LP,[],N,CN,TFin,PD) :-
   eliminarElementos(LP,LP,N,CN,TFin,PD).
-% CASO: Fin del Eliminado
-eliminarElementos(_,CLP,_,_,TFin,CLP) :-
-  length(CLP,TCLP),
-  TCLP =:= TFin, !.
-% CASO: Contador a 0
-eliminarElementos(_,LP,N,0,TFin,PD) :-
-  eliminarElementos(LP,LP,N,N,TFin,PD).
 % CASO: Avance
 eliminarElementos(LP,[_|CLPs],N,CN,TFin,PD) :-
   Nuevo is CN - 1,
+  Nuevo > 0,
   eliminarElementos(LP,CLPs,N,Nuevo,TFin,PD).
+
+eliminarElegido(LP1,[E|LP2],LF) :-
+  reverse(LP1,RLP1),
+  length([E|LP2],TLP2),
+  eliminarNElementos(RLP1,TLP2,RLF),
+  reverse(RLF,LF).
+
+eliminarNElementos(X,0,X).
+eliminarNElementos([_|Xs],N,R) :-
+  Nuevo is N - 1,
+  eliminarNElementos(Xs,Nuevo,R).
+%remove_first_X(X,[X|Xs],Xs).
+%remove_first_X(X,[Y|Xs],[Y|Ys]) :-
+%  X \= Y,
+%  remove_first_X(X,Xs,Ys).
+
+% Pruebas
+
+% ?- eliminarElementos([a,b,c,d],[c,d],3,3,2,X).
+
+% ? - sortear([a,b,c,d,e,f],3,N,P,[a,c,d]).
+%
+% N = 3
+% P = 3
+
+% ?- sortear([m,m,h,m,h,h,m,h,m,h],20,N,P,[h,h,h,h,h]).
+%
+% N = 11
+% P = 2
+
+% ?- sortear([m,m,h,m,h,h,m,h,m,h],30,N,P,[m,m,m,m,m]).
+%
+% N = 29
+% P = 10
