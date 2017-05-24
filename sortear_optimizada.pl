468a3ccbe4abbe7852fe54c11dbfc7ce77638a2f
DanielMChaves <daniel.melero.95@gmail.com>
2017-05-24 15:49:05 +0200

    Optimization Version

--- sortear_optimizada.pl
+++ sortear_optimizada.pl
@@ -15,9 +15,7 @@ sortear(Ini,C,N,P,Fin) :-
   integer(C),
   C > 0,
   length(Ini,TIni),
-  generarP(TIni,LP),
-  generarN(C,LN),
-  productoCartesiano(LP,LN,LCartesiano),
+  findall([X,Y],(between(1,TIni,X),between(1,C,Y)),LCartesiano),
   length(Fin,TFin),
   obtenerSolucion(Ini,Fin,LCartesiano,TFin,P,N).
 
@@ -28,35 +26,6 @@ esLista(X) :-
 esLista([]).
 esLista([_|T]) :-
         esLista(T).
-
-% generarP/2: Genera una lista con los valores que puede tomar P
-generarP(C,LN) :-
-  generarPAux(C,1,LN).
-
-generarPAux(C,C,[C]) :- !.
-generarPAux(C,Valor,[Valor|LNs]) :-
-  Nuevo is Valor + 1,
-  generarPAux(C,Nuevo,LNs).
-
-% generarN/2: Genera una lista con los valores que puede tomar N
-generarN(C,LN) :-
-  generarNAux(C,1,LN).
-
-generarNAux(C,C,[C]) :- !.
-generarNAux(C,Valor,[Valor|LNs]) :-
-  Nuevo is Valor + 1,
-  generarNAux(C,Nuevo,LNs).
-
-% productoCartesiano/3: Genera una lista de listas con los productos
-% cartesianos de P y N
-productoCartesiano(L1,L2,L3):-
-  productoCartesianoAux(L1,L2,L3,L1).
-
-productoCartesianoAux(_,[],[],_).
-productoCartesianoAux([],[_|T2],L3,L4):-
-    productoCartesianoAux(L4,T2,L3,L4).
-productoCartesianoAux([H1|T1],[H2|T2],[[H1,H2]|T3],L4):-
-  productoCartesianoAux(T1,[H2|T2],T3,L4).
 
 % obtenerSolucion/6: Genera y comprueba las soluciones
 obtenerSolucion(Ini,Fin,[[P,N]|_],TFin,P,N) :-
